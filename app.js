7ec258c8e92055512c7aa99aaf231a5534e6414a
Tanish Rastogi <67361189+tanishrastogi@users.noreply.github.com>
2024-07-02 16:57:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,12 @@ const todoListSchema = new mongoose.Schema({
     name:String
 })
 
+const blogSchema = new mongoose.Schema({
+    title:String,
+    content:String
+})
 
+const Blog = mongoose.model("blog", blogSchema);
 
 const todoListItem = mongoose.model("listItem" , todoListSchema);
 
@@ -145,10 +150,9 @@ app.get("/lists/:name" , function(req,res){
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts= []; 
 
-app.get("/blog" , function(req,res){
-    
+app.get("/blog" ,async function(req,res){
+    const posts = await Blog.find({})
     res.render(__dirname+"/views/ejsFiles/home" , {homeContent : homeStartingContent , posts : posts}); 
 } )
 
@@ -164,21 +168,28 @@ app.get("/blog/compose" , function(req,res){
     res.render(__dirname+"/views/ejsFiles/compose");
 })
 
-app.post("/blog/compose" , function(req,res){
+app.post("/blog/compose" ,async function(req,res){
 
     var post = {
         title: req.body.postTitle,
         content: req.body.postBody
     }
-    posts.push(post); 
+
+    const blog = new Blog({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    })
+
+    await blog.save();
     res.redirect("/blog");
 })
 
-app.get("/blog/posts/:name" , function(req,res){
+app.get("/blog/posts/:name" , async function(req,res){
     // console.log(req.params.name);
     const requiredTitle = req.params.name;
     // requiredTitle =  _.lowerCase(requiredTitle);
     console.log(requiredTitle);
+    const posts = await Blog.find({});
     posts.forEach(function(post){
         let storedTitle = post.title;
         storedTitle = _.lowerCase(storedTitle);
